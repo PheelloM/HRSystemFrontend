07b560235a8ce354634412bfa79a5898e8407698
Siya Nkosi <ntokozonkosi07@gmail.com>
2021-03-14 17:26:51 +0200

    fixed popup

--- src/app/declaration/declaration.component.ts
+++ src/app/declaration/declaration.component.ts
@@ -80,43 +80,43 @@ isSubmitted = false;
           }
 
    // validateAnswers();
-   this.declObj.question1=this.declairationForm.value.question1;
-   this.declObj.question2=this.declairationForm.value.question2;
-   this.declObj.question3=this.declairationForm.value.question3;
-   this.declObj.question4=this.declairationForm.value.question4;
-   this.declObj.question5=this.declairationForm.value.question5;
-   this.declObj.question6=this.declairationForm.value.question6;
-   this.declObj.question7=this.declairationForm.value.question7;
-   this.declObj.question8=this.declairationForm.value.question8;
-   this.declObj.question9=this.declairationForm.value.question9;
-   this.declObj.question10=this.declairationForm.value.question10;
-   this.declObj.question11=this.declairationForm.value.question11;
-   this.declObj.question12=this.declairationForm.value.question12;
-   this.declObj.question13=this.declairationForm.value.question13;
-   this.declObj.question14=this.declairationForm.value.question14;
-   this.declObj.question15=this.declairationForm.value.question15;
-   this.declObj.question16=this.declairationForm.value.question16;
-   this.declObj.question17=this.declairationForm.value.question17;
+  //  this.declObj.question1=this.declairationForm.value.question1;
+  //  this.declObj.question2=this.declairationForm.value.question2;
+  //  this.declObj.question3=this.declairationForm.value.question3;
+  //  this.declObj.question4=this.declairationForm.value.question4;
+  //  this.declObj.question5=this.declairationForm.value.question5;
+  //  this.declObj.question6=this.declairationForm.value.question6;
+  //  this.declObj.question7=this.declairationForm.value.question7;
+  //  this.declObj.question8=this.declairationForm.value.question8;
+  //  this.declObj.question9=this.declairationForm.value.question9;
+  //  this.declObj.question10=this.declairationForm.value.question10;
+  //  this.declObj.question11=this.declairationForm.value.question11;
+  //  this.declObj.question12=this.declairationForm.value.question12;
+  //  this.declObj.question13=this.declairationForm.value.question13;
+  //  this.declObj.question14=this.declairationForm.value.question14;
+  //  this.declObj.question15=this.declairationForm.value.question15;
+  //  this.declObj.question16=this.declairationForm.value.question16;
+  //  this.declObj.question17=this.declairationForm.value.question17;
 
-   this.formdataObj.declarations=this.declObj;
-   this.service.setFormDataDecl(this.formdataObj.declarations);
+  //  this.formdataObj.declarations=this.declObj;
+  //  this.service.setFormDataDecl(this.formdataObj.declarations);
 
-   console.log('Object Details: '+ this.service.getFormDataDetail());
-    console.log('Object Declaration: '+this.declObj);
-    console.log(JSON.stringify(this.service.getFormDataHome()));
-     console.log(JSON.stringify(this.service.getFormDataDetail()));
-      console.log(JSON.stringify(this.declObj));
+  //  console.log('Object Details: '+ this.service.getFormDataDetail());
+  //   console.log('Object Declaration: '+this.declObj);
+  //   console.log(JSON.stringify(this.service.getFormDataHome()));
+  //    console.log(JSON.stringify(this.service.getFormDataDetail()));
+  //     console.log(JSON.stringify(this.declObj));
 
 
 
-       //     this._api.saveDeclaration(this.declairationForm.value)
-              //this._api.saveDeclaration(JSON.stringify(this.declObj,JSON.stringify(this.service.getFormDataDetail())
-       //         .subscribe(x => {
-       //           debugger;
-       //           this.router.navigate(['/declaration', this.declairationForm]);
-       //         }, e => {
-        //          debugger;
-        //        })
+           this._api.saveDeclaration(this.declairationForm.value)
+              // this._api.saveDeclaration(JSON.stringify(this.declObj,JSON.stringify(this.service.getFormDataDetail())
+               .subscribe(x => {
+                 debugger;
+                 this.router.navigate(['/home', this.declairationForm]);
+               }, e => {
+                 debugger;
+               })
 
 
       /*if(!this.declObj.question1 || !this.declObj.question2 || !this.declObj.question3
